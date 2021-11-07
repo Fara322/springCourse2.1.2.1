6e8fee3a1648a836c14eb0c7c8fdbb41d57c5306
Фарид <85080879+Fara322@users.noreply.github.com>
2021-11-07 14:24:05 +0300

    Добавлен новый класс Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean examination1 = bean1 == bean2;
+        boolean examination2 = beanCat1 == beanCat2;
+
+        System.out.println(examination1);
+        System.out.println(examination2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+    private String name;
+
+    public Cat(){
+
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getCat() {
+        return name;
+    }
+
+    public void setCat(String name) {
+        this.name = name;
+    }
+}
